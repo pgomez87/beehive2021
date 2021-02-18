dae84b253778015bf5717785a94148ef64612346
JPablo <JPablo@MacBook-de-Pablo.local>
2021-02-18 17:30:02 -0600

    Project initialization

---
+++ beehive/js/models/bee.js
@@ -0,0 +1,16 @@
+// class Bee {
+//     constructor(id, name, username, email, address, isOwner, phone, website, company) {
+//         this.id = id;
+//         this.name = name;
+//         this.username = username;
+//         this.email = email;
+//         this.address = address;
+//         this.isOwner = isOwner;
+//         this.phone = phone;
+//         this.website = website;
+//         this.company = company;
+//         this.post = [];
+//         this.todos = [];
+//         this.albums = [];
+//     }
+// }
